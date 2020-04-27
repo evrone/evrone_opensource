b2ccca14f66089f1bdd4f12961515fbe5b1a1b51
Boris Kuznetsov <me@achempion.com>
2020-04-27 14:25:08 +0300

    correct all rubocop issues

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
+# Common helpers to templates
 module ApplicationHelper
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Common configuration for models
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
