1140467ce28303a4781d8c8ffe36ca3e0adef5f7
Boris Kuznetsov <me@achempion.com>
2021-02-08 13:57:03 +0300

    Formating

--- test/integration/readme_cases_test.rb
+++ test/integration/readme_cases_test.rb
@@ -1,17 +1,19 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class ReadmeCasesTest < ActiveSupport::TestCase
   FILES = [
-    "test/fixtures/files/readmes/thoughtbot-paul-revere",
-    "test/fixtures/files/readmes/maetl-calyx"
+    'test/fixtures/files/readmes/thoughtbot-paul-revere',
+    'test/fixtures/files/readmes/maetl-calyx'
   ].freeze
 
-  test "converts readmes successfuly" do
+  test 'converts readmes successfuly' do
     Dir.mktmpdir do |dir|
       FILES.each do |file|
         file_before = Rails.root.join("#{file}--before.md")
         file_after = Rails.root.join("#{file}--after.md")
-        modified_file = [dir, "/README.md"].join
+        modified_file = [dir, '/README.md'].join
 
         FileUtils.cp(file_before, modified_file)
 
